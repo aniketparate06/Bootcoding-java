f53e19c89113bc5459080a63c8ed2f4ace3031c9
Aniket P <aniketparate623@gmail.com>
2023-05-16 22:58:36 -0700

    Write a Java program to print the area and perimeter of a rectangle.

---
+++ src/main/java/com/bootcoding_java/Assignment_2.java
@@ -0,0 +1,17 @@
+package com.bootcoding_java;
+
+public class Assignment_2 {
+    public static void main(String[] args) {
+        float width= 5.6f;
+        float height =8.5f;
+
+        //Area of rectangle
+        float areaOfRectangle=width*height;
+        System.out.println("area of rectangle is "+width +" * "+height+" = "+areaOfRectangle);
+
+        //Perimeter of rectangle
+        float perimeterOfRectangle=2*(width+height);
+        System.out.println("perimeter of rectangle is "+width +" * "+height+" = "+perimeterOfRectangle);
+    }
+
+}
