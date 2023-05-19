89f2bda49bfb9981bb833393d763d79aaefcaf2b
Aniket P <aniketparate623@gmail.com>
2023-05-19 11:23:25 +0530

    Created n terms of natural numbers and their sum

---
+++ src/main/java/com/bootcoding_java/Assignment_5.java
@@ -0,0 +1,15 @@
+package com.bootcoding_java;
+
+public class Assignment_5 {
+    public static void main(String[] args) {
+    int num=7;
+    int sum = 0;
+        System.out.println("The first 7 natural number is :");
+    for(int i=1; i<=num; i++){
+      sum=sum+i;
+        System.out.println(i);
+
+    }
+        System.out.println("The Sum of Natural Number upto 7 terms : "+sum);
+    }
+}
