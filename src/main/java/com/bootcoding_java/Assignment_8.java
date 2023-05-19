209feb529272b8e99a144478b7e1d68abc42a2a4
Aniket P <aniketparate623@gmail.com>
2023-05-19 11:54:39 +0530

    created pattern 1 12 123 1234

---
+++ src/main/java/com/bootcoding_java/Assignment_8.java
@@ -0,0 +1,16 @@
+package com.bootcoding_java;
+
+public class Assignment_8 {
+    public static void main(String[] args) {
+        int num = 4;
+        for (int i = 1; i <= num; i++) {
+            {
+                int j;
+                for (j = 1; j <= i; j++) {
+                    System.out.print(j);
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+}
