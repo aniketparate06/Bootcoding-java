10df11d68d458d53bd7bc91e7d20455f4faa3f94
Aniket P <aniketparate623@gmail.com>
2023-05-19 11:55:30 +0530

    created pattern  1  22  333  4444

---
+++ src/main/java/com/bootcoding_java/Assignment_9.java
@@ -0,0 +1,13 @@
+package com.bootcoding_java;
+
+public class Assignment_9 {
+    public static void main(String[] args) {
+        int num = 4;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
+    }
+}
