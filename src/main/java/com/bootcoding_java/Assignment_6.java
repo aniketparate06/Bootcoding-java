b1fd6e0ecc234cfc15b49698ea92519972d30cf5
Aniket P <aniketparate623@gmail.com>
2023-05-19 11:31:43 +0530

    Created basic array program

---
+++ src/main/java/com/bootcoding_java/Assignment_6.java
@@ -0,0 +1,10 @@
+package com.bootcoding_java;
+
+public class Assignment_6 {
+    public static void main(String[] args) {
+        int num[]={5,7,8,9,11,15};
+        for (int i=0; i<num.length; i++){
+            System.out.println(num[i]);
+        }
+    }
+}
