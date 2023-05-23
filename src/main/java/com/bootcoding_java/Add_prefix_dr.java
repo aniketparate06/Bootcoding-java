3f00013b3120b11fedce79b23a6fe74d8cc2f370
Aniket P <aniketparate623@gmail.com>
2023-05-23 10:21:57 +0530

    Add prefix dr.

---
+++ src/main/java/com/bootcoding_java/Add_prefix_dr.java
@@ -0,0 +1,11 @@
+package com.bootcoding_java;
+
+public class Add_prefix_dr {
+    public static void main(String[] args) {
+        String[] names={"Rajesh", "Tushar", "Anil", "Megha"};
+        for (int i = 0; i < names.length ; i++) {
+            System.out.println("Dr. "+names[i]);
+
+        }
+    }
+}
