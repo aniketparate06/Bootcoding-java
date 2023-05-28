332c4919f9ed61bfc43326eaf53678092b786d27
Aniket P <aniketparate623@gmail.com>
2023-05-28 23:25:24 +0530

    Right angle triangle pattern * * * * * * * * * * * * * * *

---
+++ src/main/java/com/pattern/Right_angle_triangle.java
@@ -0,0 +1,12 @@
+package com.pattern;
+
+public class Right_angle_triangle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
