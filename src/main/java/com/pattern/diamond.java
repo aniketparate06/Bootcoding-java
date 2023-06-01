f69fc505b1f8ef6e65a24cf314297c6e2c20dec5
Aniket P <aniketparate623@gmail.com>
2023-06-01 11:39:00 +0530

    Diamond Pattern Program

---
+++ src/main/java/com/pattern/diamond.java
@@ -0,0 +1,29 @@
+package com.pattern;
+public class diamond {
+    public static void main(String[] args) {
+//        ***************First Half****************
+
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 5; j >i ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <=i ; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            }
+
+//        ***************Second Half******************
+
+        for (int i = 4; i >=1 ; i--) {
+            for (int j = 5; j >i ; j--){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <=i ; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
